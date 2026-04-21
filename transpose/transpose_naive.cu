6ce4e5e19a9066216c041b74f28096ffcfd873f3
huang kexin <2218887102@qq.com>
2026-04-21 16:32:45 +0800

    转置矩阵展开

---
+++ transpose/transpose_naive.cu
@@ -0,0 +1,26 @@
+#include <cuda_runtime.h>
+
+#define UNROLLING_X 4
+
+__global__ void matrix_transpose_kernel(const float* input, float* output, int nx, int ny) {
+    int ix = UNROLLING_X * blockIdx.x * blockDim.x + threadIdx.x;
+    int iy = blockIdx.y * blockDim.y + threadIdx.y;
+    for (int stride = 0; stride < UNROLLING_X; ++stride) {
+        int index_in = iy + (ix + stride * blockDim.x) * ny;
+        int index_out = ix + stride * blockDim.x + iy * nx;
+        if (ix + stride * blockDim.x < nx && iy < ny) output[index_out] = input[index_in];
+    }
+}
+
+
+
+// input, output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* input, float* output, int rows, int cols) {
+
+    dim3 threadsPerBlock(16, 16);
+    dim3 blocksPerGrid((rows - 1) / (UNROLLING_X * threadsPerBlock.x) + 1,
+                       (cols - 1) / threadsPerBlock.y + 1);
+
+    matrix_transpose_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, output, rows, cols);
+    cudaDeviceSynchronize();
+}
